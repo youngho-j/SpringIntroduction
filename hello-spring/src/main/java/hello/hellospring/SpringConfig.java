80fa4cdb85ee5b28f156b60b51d0b7130e465345
youngho-j <alfkwl1239@naver.com>
2022-05-06 14:14:28 +0900

    스프링 데이터 JPA
    1. SpringDataJpaMemberRepository 추가
     - JpaRepository<Member, Long>, MemberRepository 상속 받음
     - 기본 구현 메서드 외 findByName 메서드 추가
    
    2. SpringConfig 수정
     - 스프링 데이터 JPA 사용을 위해 설정 변경

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -6,28 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-    //메모리 - return new MemoryMemberRepository();
-    //순수 JDBC - return new JdbcMemberRepository(dataSource);
-    //JDBC Template - return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//    //메모리 - return new MemoryMemberRepository();
+//    //순수 JDBC - return new JdbcMemberRepository(dataSource);
+//    //JDBC Template - return new JdbcTemplateMemberRepository(dataSource);
+//    //JPA - return new JpaMemberRepository(em);
+//    }
 }
